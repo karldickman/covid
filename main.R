640566a249267c9a1d91506de6928c74eb174a07
Karl Dickman <karldickman@gmail.com>
2023-09-08 11:01:49 -0700

    MOre clear axis labels.

--- main.R
+++ main.R
@@ -30,7 +30,7 @@ read <- function () {
       & `WEEKLY RATE` != 'null'
     ) %>%
     transmute(
-      date = as.Date(paste0(`MMWR-YEAR`, str_pad(`MMWR-WEEK` - 1, 2, pad = "0"), "0"), "%Y%U%w") + 6,
+      date = as.Date(paste0(`MMWR-YEAR`, str_pad(`MMWR-WEEK` - 1, 2, pad = "0"), "0"), "%Y%U%w") + 6 + 7,
       `WEEKLY RATE`,
       weekly_rate = as.numeric(`WEEKLY RATE`)
     ) %>%
@@ -41,6 +41,7 @@ plot <- function (data, log_scale) {
   data %>%
     ggplot(aes(x = date, y = weekly_rate, col = risk_level)) +
     geom_hline(yintercept = breaks) +
+    scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m") +
     scale_y_continuous(trans = ifelse(log_scale, "log10", "identity")) +
     scale_color_manual(
       name = "Risk level (cases/100k)",
@@ -49,8 +50,11 @@ plot <- function (data, log_scale) {
     geom_line(color = "black") +
     geom_point() +
     ggtitle(label = 'Rates of laboratory-confirmed COVID-19-associated hospitalization', sub = 'From CDC COVID-NET surveillance data in Oregon') +
-    xlab('Date') +
-    ylab(paste('Weekly cases per 100k people', ifelse(log_scale, "(log scale)", "")))
+    xlab('Week ending') +
+    ylab(paste('Weekly cases per 100k people', ifelse(log_scale, "(log scale)", ""))) +
+    theme(
+      axis.text.x = element_text(angle = 315, hjust = 0)
+    )
 }
 
 main <- function (argv = c()) {
